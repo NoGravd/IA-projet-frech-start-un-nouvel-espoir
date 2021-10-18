7c7557ccb49356295383cf2698fe161232a38f4a
hugoapeloig <91073452+hugoapeloig@users.noreply.github.com>
2021-10-18 13:41:37 +0200

    Update Algo1.java

--- src/IA/Algo1.java
+++ src/IA/Algo1.java
@@ -9,29 +9,6 @@ public class Algo1 {
 	public static void main(String[] args) {
 		//TODO
 		//Au tout début 
-		demarrerCapteurUltraSon();
-		float[] tab = new float [1000000];
-		
-		//Dans le while
-		capteurUS.getDistanceMode().fetchSample(tab, i);
-		
-		//Après
-		eteindreCapteurUltraSon();
-		int jj=0;
-		while(tab[jj]!=(float) 0) {
-			jj++;
-		}
-		tab = Arrays.copyOf(tab, jj);
-		float min = tab[0];
-		int indicdumin = 0;
-		for (int kk=1; kk<tab.length; kk++) {
-			if (min > tab[kk]) {
-				min = tab[kk];
-				indicdumin = kk;
-			}
-		}
-		int angle = (indicdumin/tab.length)*1560;
-		Roues.mA.rotateTo(angle);
 	}
 
 }
