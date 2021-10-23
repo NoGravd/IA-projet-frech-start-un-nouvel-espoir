c51e9d05b0d949ab360e0d35337f0b588c78e910
TheoJ5 <theojlt1002@gmail.com>
2021-10-23 08:54:19 +0200

    Ajout javadoc class Pince

--- src/Le_robot/Pince.java
+++ src/Le_robot/Pince.java
@@ -4,12 +4,22 @@ import lejos.hardware.motor.BaseRegulatedMotor;
 import lejos.hardware.motor.Motor;
 import lejos.utility.Delay;
 
+/**
+Une classe qui permet de controler les pinces du robot.
+*/
 public class Pince {
+	/**
+    Instance qui permet de determiner le temps d'ouverture et de fermetur de la pince
+	 */
 	static final int TIME_PINCE = 3000; //pas touche : ca marche
+	/**
+    Instance qui represente le moteur qui controle les pinces
+	 */
 	static final BaseRegulatedMotor mP = Motor.B;//moteur pince
 	
-	
-	
+	/**
+    Ouvre la pince, que si elle est ferme.
+	 */
 	public static void oPince() {
 		if (!Memoire.getEtatPince()) {//si Pince fermees
 			mP.forward();
@@ -19,6 +29,9 @@ public class Pince {
 		}
 	}
 	
+	/**
+    Ouvre la pince, que si elle est ouverte.
+	 */
 	public static void fPince() {
 		if (Memoire.getEtatPince()) {//si Pince ouvertes
 			if (Capteur.capteurTactileActive())//si le palet
