e28cb31779b744c8cf9d0a6f8cfd11e8650a4e80
hugoapeloig <91073452+hugoapeloig@users.noreply.github.com>
2021-10-25 16:43:11 +0200

    LE TEST COULEUR
    This reverts commit 5d900cfb07d1f3e8dadc0fc4aee0e15efed0fbbb.

--- src/Le_robot/Test_Hugo_Theo.java
+++ src/Le_robot/Test_Hugo_Theo.java
@@ -1,5 +1,37 @@
 package Le_robot;
 
+import lejos.hardware.sensor.*;
+import lejos.robotics.Color;
+import lejos.utility.Delay;
+import java.util.Arrays;
+
+import lejos.hardware.BrickFinder;
+import lejos.hardware.Button;
+import lejos.hardware.port.*;
+import lejos.hardware.port.Port;
+
 public class Test_Hugo_Theo {
+	public static void main(String[] args) {
+		try {
+			
+		Port p1 = BrickFinder.getLocal().getPort("S1");
+		EV3ColorSensor capteurCo = new EV3ColorSensor(p1);
+		capteurCo.setFloodlight(true);
+		capteurCo.setFloodlight(Color.WHITE);
+		//float[] tab = new float[3];
+		//capteurCo.getRGBMode().fetchSample(tab, 0);
+		int i =capteurCo.getColorID();
+		Delay.msDelay(2000);
+		//boolean i = capteurCo.setFloodlight(1);
+		capteurCo.setFloodlight(false);
+		System.out.print(i);
+		Button.ENTER.waitForPress();//question nono : il va falloir le virer ? wesh
+		} catch (Throwable t) {
+			t.printStackTrace();
+			Delay.msDelay(10000);
+			System.exit(0);
+		}
+	}
 
 }
+
