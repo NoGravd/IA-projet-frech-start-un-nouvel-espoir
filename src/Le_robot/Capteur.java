f3cbaca4d7a106f7b2da3829e954f256257e6472
TheoJ5 <theojlt1002@gmail.com>
2021-11-20 08:54:26 +0100

    JavaDoc Capteur + modif capteur couleur
    Rajout de la javadoc qu'il manqué
    Modification de la fonction couleur detectée

--- src/Le_robot/Capteur.java
+++ src/Le_robot/Capteur.java
@@ -164,14 +164,18 @@ public class Capteur {
 	
 
 	/**
+    Place l'ID de la couleur detecter dans le tableau donneeCo.
+	 */
+	public static void couleurDetectee() {
+		
+		capteurCo.getColorID().fetchSample(donneeCo, 0);
+		
+	}
+	
+	/**
     Permet de savoir qu'elle est la couleur detecte par le robot.
     @return l'ID de la couleur detecter.
 	 */
-	public static int couleurDetectee() {
-		//Donne ID de la couleur detecter par le capteur de couleur en mode RGB
-		return capteurCo.getColorID();
-	}
-	
 	public static float getCouleur() {			//Pas utile pour le moment
 		return donneeCo [0];
 	}
