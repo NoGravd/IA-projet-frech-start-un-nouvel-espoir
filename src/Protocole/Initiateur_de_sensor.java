0307c235f93062537fd10593c8387cf67e405f52
TheoJ5 <theojlt1002@gmail.com>
2021-11-20 09:47:51 +0100

    javadoc initialisateur
    Ajout de la doc

--- src/Protocole/Initiateur_de_sensor.java
+++ src/Protocole/Initiateur_de_sensor.java
@@ -5,9 +5,15 @@ import Le_robot.Music;
 import lejos.hardware.Button;
 
 //class qui initialise les sensor et attend la presse du bouton pour terminer
+/**
+	Une classe qui initialise les sensors et attend la presse du bouton pour terminer
+ */
 public class Initiateur_de_sensor {
 	
 	
+	/**
+	Une méthode qui initialise les sensors et attend la presse du bouton pour terminer
+ */
 	public static void initialiseTout() throws InterruptedException {
 		initialise_color();
 		initialise_touch();
@@ -18,6 +24,9 @@ public class Initiateur_de_sensor {
 		Button.ENTER.waitForPress();
 	}
 	
+	/**
+	Une méthode qui initialise le capteur couleurs.
+	 */
 	public static void initialise_color() {
 		
 		capteurCo.setFloodlight(true);					// A verifier mais normalement ça marche comme ça
@@ -25,10 +34,16 @@ public class Initiateur_de_sensor {
 		
 	}
 	
+	/**
+	Une méthode qui initialise le capteur tactile.
+	 */
 	public static void initialise_touch() {
 		Capteur.capteurTactileActive();
 	}
 	
+	/**
+	Une méthode qui initialise le capteur ultrason.
+	 */
 	public static void initialise_US() {
 		Capteur.capteurUS.enable();
 	}
