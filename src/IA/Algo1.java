742dabb26c08e473f78f002e9d44295020244bcb
TheoJ5 <theojlt1002@gmail.com>
2021-11-20 08:48:25 +0100

    Java doc Algo 1
    Java doc Algo 1

--- src/IA/Algo1.java
+++ src/IA/Algo1.java
@@ -2,8 +2,16 @@ package IA;
 
 import Protocole.*;
 
+/**
+Une classe qui contient toute les aglorithmes qui pouront etre utilisee lors du tournois.
+*/
 public class Algo1 {
 	
+	/**
+    Strategie la plus basic consiste a chercher un palet puis l'emporter dans la base
+    adverse et tous cela en boucle.
+    @param un tableau de String
+	 */
 	public static void Forest_Gump(String[] args) throws InterruptedException {
 		Starter.Starter1(null);
 		while (1<2) {//boucle infini tkt frero
