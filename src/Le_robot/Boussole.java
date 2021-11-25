80b99ff2903f3950a1adb2f51384e745fead5e66
NoGravd <91073435+NoGravd@users.noreply.github.com>
2021-11-25 10:28:35 +0100

    new class Boussole

---
+++ src/Le_robot/Boussole.java
@@ -0,0 +1,25 @@
+package Le_robot;
+
+public class Boussole {
+	private int boussole;
+	
+	public Boussole (int bous) {
+		boussole = bous;
+	}
+	
+	/*
+	  Modifie les valeurs des boussoles car le robot et en train de pivoter de [int degre].
+	  @param degre Le degre de rotation du robot
+	 */
+	public void rotateDeg (int degre) {
+		boussole += degre;
+		if (boussole>=360)
+			boussole -= 360;
+		else if (boussole<=0)
+			boussole += 360;
+	}
+	
+	public int get() {
+		return boussole;
+	}
+}
